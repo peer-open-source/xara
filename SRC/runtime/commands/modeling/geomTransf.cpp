35e995ce6b5cd6abebf58945e68cac0b9c14ec6f
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-03-25 17:33:18 -0700

    Clean up errors

--- SRC/runtime/commands/modeling/geomTransf.cpp
+++ SRC/runtime/commands/modeling/geomTransf.cpp
@@ -20,7 +20,6 @@
 #include <PDeltaCrdTransf3d.h>
 #include <CorotCrdTransf2d.h>
 #include <CorotCrdTransf3d.h>
-//#include <LinearCrdTransf2d02.h>
 #include <LinearCrdTransf2dInt.h>
 #include <CorotCrdTransfWarping2d.h>
 
@@ -42,31 +41,29 @@ TclCommand_addGeomTransf(ClientData clientData, Tcl_Interp *interp, int argc,
 
   // Make sure there is a minimum number of arguments
   if (argc < 2) {
-    opserr << G3_ERROR_PROMPT << "insufficient number of geomTransf arguments\n";
-    opserr << "    Expected: geomTransf type? tag? <specific transf args>\n";
+    opserr << G3_ERROR_PROMPT << "insufficient number of arguments\n";
     return TCL_ERROR;
   }
 
   int ndm = builder->getNDM();
   int ndf = builder->getNDF(); // number of degrees of freedom per node
-
-  // create 2d coordinate transformation
+  //
+  // 2D Case
+  //
   if ((ndm == 2 && ndf == 3) || (ndm == 2 && ndf == 4)) {
 
-    int crdTransfTag;
+    int tag;
     Vector jntOffsetI(2),
            jntOffsetJ(2);
 
     if (argc < 3) {
-      opserr << G3_ERROR_PROMPT << "insufficient arguments\n"
-        "    Expected: geomTransf type? tag? <-jntOffset dXi? dYi? dXj? dYj?>\n";
+      opserr << G3_ERROR_PROMPT << "insufficient arguments\n";
       return TCL_ERROR;
     }
 
     int argi = 2;
-    if (Tcl_GetInt(interp, argv[argi++], &crdTransfTag) != TCL_OK) {
-      opserr << G3_ERROR_PROMPT << "invalid tag\n"
-        "    Expected: geomTransf type? tag? <-jntOffset dXi? dYi? dXj? dYj?>\n";
+    if (Tcl_GetInt(interp, argv[argi++], &tag) != TCL_OK) {
+      opserr << G3_ERROR_PROMPT << "invalid tag\n";
       return TCL_ERROR;
     }
 
@@ -78,8 +75,7 @@ TclCommand_addGeomTransf(ClientData clientData, Tcl_Interp *interp, int argc,
         for (int i = 0; i < 2; ++i) {
           if (argi == argc ||
               Tcl_GetDouble(interp, argv[argi++], &jntOffsetI(i)) != TCL_OK) {
-            opserr << G3_ERROR_PROMPT << "invalid jntOffset value\n"
-              "    Expected: geomTransf type? tag? <-jntOffset dXi? dYi? dXj? dYj?>\n";
+            opserr << G3_ERROR_PROMPT << "invalid jntOffset value\n";
             return TCL_ERROR;
           }
         }
@@ -87,75 +83,66 @@ TclCommand_addGeomTransf(ClientData clientData, Tcl_Interp *interp, int argc,
         for (int i = 0; i < 2; ++i) {
           if (argi == argc ||
               Tcl_GetDouble(interp, argv[argi++], &jntOffsetJ(i)) != TCL_OK) {
-            opserr << G3_ERROR_PROMPT << "invalid jntOffset value\n"
-              "    Expected: geomTransf type? tag? <-jntOffset dXi? dYi? dXj? dYj?>\n";
+            opserr << G3_ERROR_PROMPT << "invalid jntOffset value\n";
             return TCL_ERROR;
           }
         }
       }
 
       else {
-        opserr << G3_ERROR_PROMPT << "bad command\n    Expected: geomTransf type? tag? "
-                  "<-jntOffset dXi? dYi? dXj? dYj?>\n";
-        opserr << "invalid: " << argv[argi] << "\n";
+        opserr << G3_ERROR_PROMPT << "unexpected argument " << argv[argi] << "\n";
         return TCL_ERROR;
       }
     }
 
+    //
     // construct the transformation
+    //
 
     FrameTransform2d *crdTransf2d = nullptr;
 
     if (strcmp(argv[1], "Linear") == 0)
-//    if (getenv("CRD")) {
-//      crdTransf2d = new LinearCrdTransf2d02(crdTransfTag, jntOffsetI, jntOffsetJ);
-//    } else
-        crdTransf2d = new LinearCrdTransf2d(crdTransfTag, jntOffsetI, jntOffsetJ);
+        crdTransf2d = new LinearCrdTransf2d(tag, jntOffsetI, jntOffsetJ);
 
     else if (strcmp(argv[1], "LinearInt") == 0)
       crdTransf2d =
-          new LinearCrdTransf2dInt(crdTransfTag, jntOffsetI, jntOffsetJ);
+          new LinearCrdTransf2dInt(tag, jntOffsetI, jntOffsetJ);
 
     else if (strcmp(argv[1], "PDelta") == 0 ||
              strcmp(argv[1], "LinearWithPDelta") == 0)
-      crdTransf2d = new PDeltaCrdTransf2d(crdTransfTag, jntOffsetI, jntOffsetJ);
+      crdTransf2d = new PDeltaCrdTransf2d(tag, jntOffsetI, jntOffsetJ);
 
     else if (strcmp(argv[1], "Corotational") == 0 && ndf == 3)
-      crdTransf2d = new CorotCrdTransf2d(crdTransfTag, jntOffsetI, jntOffsetJ);
+      crdTransf2d = new CorotCrdTransf2d(tag, jntOffsetI, jntOffsetJ);
 
     else if (strcmp(argv[1], "Corotational") == 0 && ndf == 4)
       crdTransf2d =
-          new CorotCrdTransfWarping2d(crdTransfTag, jntOffsetI, jntOffsetJ);
+          new CorotCrdTransfWarping2d(tag, jntOffsetI, jntOffsetJ);
 
     else {
-      opserr << G3_ERROR_PROMPT << "invalid Type\n";
-      opserr << argv[1] << "\n";
+      opserr << G3_ERROR_PROMPT << "invalid Type: " << argv[1] << "\n";
       return TCL_ERROR;
     }
 
-    // add the transformation to the modelBuilder
+    //
     if (builder->addTaggedObject<FrameTransform2d>(*crdTransf2d) != TCL_OK)
       return TCL_ERROR;
+  }
 
-  } else if (ndm == 3 && ndf >= 6) {
-    int crdTransfTag;
+  else if (ndm == 3 && ndf >= 6) {
+    int tag;
     Vector vecxzPlane(3);                // vector that defines local xz plane
     Vector jntOffsetI(3), jntOffsetJ(3); // joint offsets in global coordinates
 
     if (argc < 6) {
       opserr << G3_ERROR_PROMPT 
-             << "insufficient arguments\n"
-             << "    Expected: geomTransf type? tag? "
-                "vecxzPlaneX? vecxzPlaneY? vecxzPlaneZ?  <-jntOffset dXi? dYi? "
-                "dZi? dXj? dYj? dZj? >\n";
+             << "insufficient arguments\n";
       return TCL_ERROR;
     }
 
     int argi = 2;
-    if (Tcl_GetInt(interp, argv[argi++], &crdTransfTag) != TCL_OK) {
-      opserr << G3_ERROR_PROMPT << "invalid tag\n    Expected: geomTransf type? tag? "
-                "vecxzPlaneX? vecxzPlaneY? vecxzPlaneZ?  <-jntOffset dXi? dYi? "
-                "dZi? dXj? dYj? dZj? >\n";
+    if (Tcl_GetInt(interp, argv[argi++], &tag) != TCL_OK) {
+      opserr << G3_ERROR_PROMPT << "invalid tag\n";
       return TCL_ERROR;
     }
 
@@ -179,28 +166,24 @@ TclCommand_addGeomTransf(ClientData clientData, Tcl_Interp *interp, int argc,
 
     if (!parsed_xz) {
       if (Tcl_GetDouble(interp, argv[argi++], &vecxzPlane(0)) != TCL_OK) {
-        opserr << G3_ERROR_PROMPT << "invalid vecxzPlaneX\n    Expected: geomTransf type? tag? "
-                  "vecxzPlaneX? vecxzPlaneY? vecxzPlaneZ?  <-jntOffset dXi? dYi? "
-                  "dZi? dXj? dYj? dZj? >\n";
+        opserr << G3_ERROR_PROMPT << "invalid vecxzPlaneX\n";
         return TCL_ERROR;
       }
 
       if (Tcl_GetDouble(interp, argv[argi++], &vecxzPlane(1)) != TCL_OK) {
-        opserr << G3_ERROR_PROMPT << "invalid vecxzPlaneY\n    Expected: geomTransf type? tag? "
-                  "vecxzPlaneX? vecxzPlaneY? vecxzPlaneZ?  <-jntOffset dXi? dYi? "
-                  "dZi? dXj? dYj? dZj? >\n";
+        opserr << G3_ERROR_PROMPT << "invalid vecxzPlaneY\n";
         return TCL_ERROR;
       }
 
       if (Tcl_GetDouble(interp, argv[argi++], &vecxzPlane(2)) != TCL_OK) {
-        opserr << G3_ERROR_PROMPT << "invalid vecxzPlaneZ\n    Expected: geomTransf type? tag? "
-                  "vecxzPlaneX? vecxzPlaneY? vecxzPlaneZ?  <-jntOffset dXi? dYi? "
-                  "dZi? dXj? dYj? dZj? >\n";
+        opserr << G3_ERROR_PROMPT << "invalid vecxzPlaneZ\n";
         return TCL_ERROR;
       }
     }
 
-    // additional keyword options at end of command
+    //
+    // Additional keyword options
+    //
 
     while (argi != argc) {
       if (strcmp(argv[argi], "-jntOffset") == 0) {
@@ -208,9 +191,7 @@ TclCommand_addGeomTransf(ClientData clientData, Tcl_Interp *interp, int argc,
         for (int i = 0; i < 3; ++i) {
           if (argi == argc ||
               Tcl_GetDouble(interp, argv[argi++], &jntOffsetI(i)) != TCL_OK) {
-            opserr << G3_ERROR_PROMPT << "invalid jntOffset value\n    Expected: geomTransf "
-                      "type? tag? vecxzPlaneX? vecxzPlaneY? vecxzPlaneZ?  "
-                      "<-jntOffset dXi? dYi? dZi? dXj? dYj? dZj? >\n";
+            opserr << G3_ERROR_PROMPT << "invalid jntOffset\n";
             return TCL_ERROR;
           }
         }
@@ -218,45 +199,42 @@ TclCommand_addGeomTransf(ClientData clientData, Tcl_Interp *interp, int argc,
         for (int i = 0; i < 3; ++i) {
           if (argi == argc ||
               Tcl_GetDouble(interp, argv[argi++], &jntOffsetJ(i)) != TCL_OK) {
-            opserr << G3_ERROR_PROMPT << "invalid jntOffset value\n    Expected: geomTransf "
-                      "type? tag? vecxzPlaneX? vecxzPlaneY? vecxzPlaneZ?  "
-                      "<-jntOffset dXi? dYi? dZi? dXj? dYj? dZj? >\n";
+            opserr << G3_ERROR_PROMPT << "invalid jntOffset\n";
             return TCL_ERROR;
           }
         }
       } else {
-        opserr << G3_ERROR_PROMPT << "bad command\n    Expected: geomTransf type? tag? "
-                  "vecxzPlaneX? vecxzPlaneY? vecxzPlaneZ?  <-jntOffset dXi? "
-                  "dYi? dZi? dXj? dYj? dZj? > \n";
-        opserr << "    invalid: " << argv[argi] << "\n";
+        opserr << G3_ERROR_PROMPT << "unexpected argument: " << argv[argi] << "\n";
         return TCL_ERROR;
       }
     }
 
-    // construct the transformation object
+    //
+    // construct the transformation
+    //
     FrameTransform3d *crdTransf3d=nullptr;
 
     if (strcmp(argv[1], "Linear") == 0)
       if (!getenv("CRD"))
-        crdTransf3d = new LinearFrameTransf3d(crdTransfTag, vecxzPlane, jntOffsetI, jntOffsetJ);
+        crdTransf3d = new LinearFrameTransf3d(tag, vecxzPlane, jntOffsetI, jntOffsetJ);
       else
-        crdTransf3d = new LinearCrdTransf3d(crdTransfTag, vecxzPlane, jntOffsetI, jntOffsetJ);
+        crdTransf3d = new LinearCrdTransf3d(tag, vecxzPlane, jntOffsetI, jntOffsetJ);
 
     else if (strcmp(argv[1], "PDelta") == 0 ||
              strcmp(argv[1], "LinearWithPDelta") == 0)
       if (!getenv("CRD"))
-        crdTransf3d = new PDeltaFrameTransf3d(crdTransfTag, vecxzPlane, jntOffsetI, jntOffsetJ);
+        crdTransf3d = new PDeltaFrameTransf3d(tag, vecxzPlane, jntOffsetI, jntOffsetJ);
       else
-        crdTransf3d = new PDeltaCrdTransf3d(crdTransfTag, vecxzPlane, jntOffsetI, jntOffsetJ);
+        crdTransf3d = new PDeltaCrdTransf3d(tag, vecxzPlane, jntOffsetI, jntOffsetJ);
 
     else if (strcmp(argv[1], "Corotational") == 0)
-      // By default use new faster version
-      if (!getenv("CRD"))
-        crdTransf3d = new CorotFrameTransf3d(crdTransfTag, vecxzPlane, jntOffsetI, jntOffsetJ);
+      if (getenv("CRD03")) {
+        crdTransf3d = new CorotFrameTransf3d03(tag, vecxzPlane, jntOffsetI, jntOffsetJ);
+      }
       else
-        crdTransf3d = new CorotFrameTransf3d03(crdTransfTag, vecxzPlane, jntOffsetI, jntOffsetJ);
+        crdTransf3d = new CorotFrameTransf3d(tag, vecxzPlane, jntOffsetI, jntOffsetJ);
 //    else
-//      crdTransf3d = new CorotCrdTransf3d(crdTransfTag, vecxzPlane, jntOffsetI, jntOffsetJ);
+//      crdTransf3d = new CorotCrdTransf3d(tag, vecxzPlane, jntOffsetI, jntOffsetJ);
 
     else {
       opserr << G3_ERROR_PROMPT << "invalid Type\n";
@@ -270,13 +248,14 @@ TclCommand_addGeomTransf(ClientData clientData, Tcl_Interp *interp, int argc,
 
     // add the transformation to the modelBuilder
     if (builder->addTaggedObject<FrameTransform3d>(*crdTransf3d) != TCL_OK) {
-      opserr << G3_ERROR_PROMPT << "could not add "
-                "geometric transformation to model Builder\n";
+      opserr << G3_ERROR_PROMPT 
+             << "Failed to add transformation to model\n";
       return TCL_ERROR;
     }
 
   } else {
-    opserr << G3_ERROR_PROMPT << "ndm = " << ndm << " and ndf = " << ndf
+    opserr << G3_ERROR_PROMPT 
+           << "ndm = " << ndm << " and ndf = " << ndf
            << " is incompatible with available frame elements\n";
     return TCL_ERROR;
   }
