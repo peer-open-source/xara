d5813d217f45f23431d49e3cea1ace2d76d878bb
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-04-15 01:07:29 -0700

    Update ElementalLoadIter.cpp

--- SRC/domain/load/ElementalLoadIter.cpp
+++ SRC/domain/load/ElementalLoadIter.cpp
@@ -1,28 +1,9 @@
-/* ****************************************************************** **
-**    OpenSees - Open System for Earthquake Engineering Simulation    **
-**          Pacific Earthquake Engineering Research Center            **
-**                                                                    **
-**                                                                    **
-** (C) Copyright 1999, The Regents of the University of California    **
-** All Rights Reserved.                                               **
-**                                                                    **
-** Commercial use of this program without express permission of the   **
-** University of California, Berkeley, is strictly prohibited.  See   **
-** file 'COPYRIGHT'  in main directory for information on usage and   **
-** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
-**                                                                    **
-** Developed by:                                                      **
-**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
-**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
-**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
-**                                                                    **
-** ****************************************************************** */
-                                                                        
-// $Revision: 1.1.1.1 $
-// $Date: 2000-09-15 08:23:19 $
-// $Source: /usr/local/cvs/OpenSees/SRC/domain/load/ElementalLoadIter.cpp,v $
-                                                                        
-                                                                        
+//===----------------------------------------------------------------------===//
+//
+//        OpenSees - Open System for Earthquake Engineering Simulation    
+//
+//===----------------------------------------------------------------------===//
+//
 // File: ~/OOP/domain/loadcase/ElementalLoadIter.C
 //
 // Written: fmk 
@@ -40,9 +21,6 @@
 #include <TaggedObjectStorage.h>
 
 
-// ElementalLoadIter(SingleDomain &theDomain):
-//	constructor that takes the model, just the basic iter
-
 ElementalLoadIter::ElementalLoadIter(TaggedObjectStorage *theStorage)
   :myIter(theStorage->getComponents())
 {
@@ -54,23 +32,23 @@ ElementalLoadIter::~ElementalLoadIter()
 }    
 
 void
-ElementalLoadIter::reset(void)
+ElementalLoadIter::reset()
 {
     myIter.reset();
 }    
 
 
 ElementalLoad *
-ElementalLoadIter::operator()(void)
+ElementalLoadIter::operator()()
 {
     // check if we still have elements in the model
     // if not return 0, indicating we are done
     TaggedObject *theComponent = myIter();
     if (theComponent == 0)
-	return 0;
+        return 0;
     else {
-	ElementalLoad *result = (ElementalLoad *)theComponent;
-	return result;
+        ElementalLoad *result = (ElementalLoad *)theComponent;
+        return result;
     }
 }
 
