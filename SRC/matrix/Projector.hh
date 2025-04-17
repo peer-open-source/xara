6eaee931221454a514e8f90a489896e5e3ff444d
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-04-16 22:54:58 -0700

    cmp - run workflow on main push

--- SRC/matrix/Projector.hh
+++ SRC/matrix/Projector.hh
@@ -1,88 +1,87 @@
-Project::Deviator
-Project::Spherical
+//===----------------------------------------------------------------------===//
+//
+//        OpenSees - Open System for Earthquake Engineering Simulation    
+//
+//===----------------------------------------------------------------------===//
+//
+#include <MatrixND.h>
 
-// from DruckerPrager.cpp
+namespace OpenSees {
 
     // 2nd order Identity Tensor
-    mI1.Zero();
-    mI1(0) = 1;
-    mI1(1) = 1;
-    mI1(2) = 1;
+    static constexpr MatrixND<3,3> I1 {{
+        {1.0, 0.0, 0.0},
+        {0.0, 1.0, 0.0},
+        {0.0, 0.0, 1.0}}};
 
-    // 4th order Volumetric Tensor
+    // 4th order mixed variant identity tensor (51b)
+    static constexpr MatrixND<6,6> IImix {{
+        {1, 0, 0,  0, 0, 0},
+        {0, 1, 0,  0, 0, 0},
+        {0, 0, 1,  0, 0, 0},
+        {0, 0, 0,  1, 0, 0},
+        {0, 0, 0,  0, 1, 0},
+        {0, 0, 0,  0, 0, 1},
+    }};
+
+    // 4th order covariant identity tensor (51a)
+    static constexpr MatrixND<6,6> IIco {{
+        {1, 0, 0,  0, 0, 0},
+        {0, 1, 0,  0, 0, 0},
+        {0, 0, 1,  0, 0, 0},
+        {0, 0, 0,  2, 0, 0},
+        {0, 0, 0,  0, 2, 0},
+        {0, 0, 0,  0, 0, 2},
+    }};
+
+    // 4th order contravariant identity tensor (51a)
+    static constexpr MatrixND<6,6> IIcon {{
+        {1, 0, 0,   0 ,  0 ,  0 },
+        {0, 1, 0,   0 ,  0 ,  0 },
+        {0, 0, 1,   0 ,  0 ,  0 },
+        {0, 0, 0,  0.5,  0 ,  0 },
+        {0, 0, 0,   0 , 0.5,  0 },
+        {0, 0, 0,   0 ,  0 , 0.5},
+    }};
+
+    // 4th order Volumetric Tensor (57)
     // IIvol = I1 tensor I1
-    mIIvol.Zero();
-    mIIvol(0,0) = 1;
-    mIIvol(0,1) = 1;
-    mIIvol(0,2) = 1;
-    mIIvol(1,0) = 1;
-    mIIvol(1,1) = 1;
-    mIIvol(1,2) = 1;
-    mIIvol(2,0) = 1;
-    mIIvol(2,1) = 1;
-    mIIvol(2,2) = 1;
+    static constexpr MatrixND<6,6> IoI {{
+        {1, 1, 1,  0, 0, 0},
+        {1, 1, 1,  0, 0, 0},
+        {1, 1, 1,  0, 0, 0},
+        {0, 0, 0,  0, 0, 0},
+        {0, 0, 0,  0, 0, 0},
+        {0, 0, 0,  0, 0, 0},
+    }};
+    static constexpr MatrixND<6,6> IIvol {{
+        {1, 1, 1,  0, 0, 0},
+        {1, 1, 1,  0, 0, 0},
+        {1, 1, 1,  0, 0, 0},
+        {0, 0, 0,  0, 0, 0},
+        {0, 0, 0,  0, 0, 0},
+        {0, 0, 0,  0, 0, 0},
+    }};
 
     // 4th order Deviatoric Tensor
+    //
     // Note:  this is the contravariant form!
     //        usable for s^a = 2G * IIdev^ab * epsilon_b
     // (Need a different form for s^a = IIdev ^a_b * sigma^a)
-    mIIdev.Zero();
-    mIIdev(0,0) = two3;
-    mIIdev(0,1) = -one3;
-    mIIdev(0,2) = -one3;
-    mIIdev(1,0) = -one3;
-    mIIdev(1,1) = two3;
-    mIIdev(1,2) = -one3;
-    mIIdev(2,0) = -one3;
-    mIIdev(2,1) = -one3;
-    mIIdev(2,2) = two3;
-    mIIdev(3,3) = 0.5;
-    mIIdev(4,4) = 0.5;
-    mIIdev(5,5) = 0.5;
+    static constexpr MatrixND<6,6> IIdev {{
+        {  2./3.,  -1./3.,  -1./3.,   0 ,  0 ,  0 },
+        { -1./3.,   2./3.,  -1./3.,   0 ,  0 ,  0 },
+        { -1./3.,  -1./3.,   2./3.,   0 ,  0 ,  0 },
+        {     0.,      0.,      0.,  0.5,  0 ,  0 },
+        {     0.,      0.,      0.,   0 , 0.5,  0 },
+        {     0.,      0.,      0.,   0 ,  0 , 0.5}
+    }};
 
-// From ManzariDafalias
-Vector              ManzariDafalias::mI1(6);
-Matrix              ManzariDafalias::mIIco(6,6);
-Matrix              ManzariDafalias::mIIcon(6,6);
-Matrix              ManzariDafalias::mIImix(6,6);
-Matrix              ManzariDafalias::mIIvol(6,6);
-Matrix              ManzariDafalias::mIIdevCon(6,6);
-Matrix              ManzariDafalias::mIIdevMix(6,6);
-Matrix              ManzariDafalias::mIIdevCo(6,6);
+    // 4th order contravariant deviatoric tensor
+    static constexpr MatrixND<6,6> IIdevCon = IIcon - 1./3.*IIvol;
+    // 4th order covariant deviatoric tensor
+    static constexpr MatrixND<6,6> IIdevCo  = IIco  - 1./3.*IIvol;
+    // 4th order mixed variant deviatoric tensor
+    static constexpr MatrixND<6,6> IIdevMix = IImix - 1./3.*IIvol;
 
-		initTensors() {
-			// 2nd order identity tensor
-			mI1.Zero();
-			mI1(0) = 1.0;
-			mI1(1) = 1.0;
-			mI1(2) = 1.0;
-			// 4th order mixed variant identity tensor
-			mIImix.Zero();
-			for (int i = 0; i<6; i++) {
-				mIImix(i,i) = 1.0;
-			}
-			// 4th order covariant identity tensor
-			mIIco = mIImix;
-			mIIco(3,3) = 2.0;
-			mIIco(4,4) = 2.0;
-			mIIco(5,5) = 2.0;
-			// 4th order contravariant identity tensor
-			mIIcon = mIImix;
-			mIIcon(3,3) = 0.5;
-			mIIcon(4,4) = 0.5;
-			mIIcon(5,5) = 0.5;
-			// 4th order volumetric tensor, IIvol = I1 tensor I1
-			mIIvol.Zero();
-			for (int i = 0; i<3; i++) {
-				mIIvol(i,0) = 1.0;
-				mIIvol(i,1) = 1.0;
-				mIIvol(i,2) = 1.0;
-			}
-			// 4th order contravariant deviatoric tensor
-			mIIdevCon = mIIcon - one3*mIIvol;
-			// 4th order covariant deviatoric tensor
-			mIIdevCo  = mIIco  - one3*mIIvol;
-			// 4th order mixed variant deviatoric tensor
-			mIIdevMix = mIImix - one3*mIIvol;
-		}
-	} initTensorOps;
+}
