5735831d8723a6b01aa57094fef02f8498187baa
peer-open-source <184699016+peer-open-source@users.noreply.github.com>
2025-04-18 20:05:27 -0700

    update SRC/material/uniaxial/JankowskiImpact.cpp

--- SRC/material/uniaxial/JankowskiImpact.cpp
+++ SRC/material/uniaxial/JankowskiImpact.cpp
@@ -54,7 +54,7 @@ void *OPS_ADD_RUNTIME_VPV(OPS_JankowskiImpact)
   // kudos
   if (numJankowskiImpact == 0) {
     numJankowskiImpact++;
-    opserr << "JankowskiImpact model written by Patrick J. Hughes, UC San Diego\n";
+    opslog << "JankowskiImpact model written by Patrick J. Hughes, UC San Diego\n";
   }
   
   // Pointer to a uniaxial material that will be returned
