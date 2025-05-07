56f5026f1bd9383044beb8b30291cc1fabf8b4d2
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-05-06 18:20:21 -0700

    Update ConstantPressureVolumeQuad.cpp

--- SRC/element/Plane/ConstantPressureVolumeQuad.cpp
+++ SRC/element/Plane/ConstantPressureVolumeQuad.cpp
@@ -8,7 +8,7 @@
 // Plane Strain (NOT PLANE STRESS)
 // 
 // Q1/P0 Mixed three-field formulation (Bbar implementation)
-// Equivalent to the mean dilation formulation.
+// Equivalent to the mean dilation formulation of Nagtegaal, Parks and Rice (1974)?
 //
 // Ed "C++" Love
 //
@@ -209,11 +209,7 @@ ConstantPressureVolumeQuad :: update( )
   double xsj ;  // determinant jacaobian matrix 
 
   static Matrix sx(2,2) ; // inverse jacobian matrix 
-
-  double theta = 0.0 ; //average volume change (trace of strain) 
-
   static Vector strain(4) ; //strain in vector form 
-
   static Vector one(4) ; //rank 2 identity as a vector
 
   //one vector
@@ -264,7 +260,7 @@ ConstantPressureVolumeQuad :: update( )
 
 
   // compute theta
-  theta = 0.0 ;
+  double theta = 0.0 ;
   for (int i = 0; i < 4; i++ ) {
 
     strain.Zero( ) ;
@@ -305,10 +301,10 @@ ConstantPressureVolumeQuad :: update( )
 
     double trace = strain(0) + strain(1) + strain(2) ;
 
-    //strain -= (one3*trace)*one ;
+    // strain -= (one3*trace)*one ;
     strain.addVector(1.0,  one, -one3*trace ) ;
 
-    //strain += (one3*theta)*one ;
+    // strain += (one3*theta)*one ;
     strain.addVector(1.0,  one, one3*theta ) ;
 
     success += materialPointers[i]->setTrialStrain( strain );
