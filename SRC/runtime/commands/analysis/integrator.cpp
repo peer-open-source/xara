8d178248a75b6ce2c281705a35bbaab5a1f82fe4
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-05-15 09:18:36 -0700

    Update banner

--- SRC/runtime/commands/analysis/integrator.cpp
+++ SRC/runtime/commands/analysis/integrator.cpp
@@ -1,9 +1,10 @@
 //===----------------------------------------------------------------------===//
 //
-//        OpenSees - Open System for Earthquake Engineering Simulation
+//                                   xara
 //
 //===----------------------------------------------------------------------===//
-//
+//                              https://xara.so
+//===----------------------------------------------------------------------===//
 // Description: This file implements selection of an integrator object.
 //
 #include <G3_Logging.h>
