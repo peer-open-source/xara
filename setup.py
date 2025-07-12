451918d6d34ddfbd9175df55a834bb689ed11fc2
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-07-11 20:18:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ import setuptools
 
 #--------------------------------------------------
 
-version    = "0.1.20"
-build_type = os.environ.get("OPENSEESRT_BUILD", "local")
+version    = "0.1.22"
+build_type = os.environ.get("OPENSEESRT_BUILD", "release")
 
 #--------------------------------------------------
 
