5cc57b1ce4a5081f7b2ec743155a2190487b406c
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-09-16 20:17:16 -0700

    fix windows

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 
 #--------------------------------------------------
 
-version    = "0.1.23"
+version    = "0.1.24"
 build_type = os.environ.get("OPENSEESRT_BUILD", "release")
 
 #--------------------------------------------------
