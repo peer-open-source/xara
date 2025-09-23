976b7d3bba7bf4afb77d7e84f8779bec77afa321
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-09-23 13:19:30 -0700

    Update TimeSeries.h

--- SRC/domain/pattern/series/TimeSeries.h
+++ SRC/domain/pattern/series/TimeSeries.h
@@ -38,9 +38,7 @@
 
 #include <TaggedObject.h>
 #include <MovableObject.h>
-#include <OPS_Globals.h>
 #include <Information.h>
-#include <elementAPI.h>
 
 class TimeSeries : public TaggedObject, public MovableObject
 {
