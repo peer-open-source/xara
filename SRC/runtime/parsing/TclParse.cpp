d096dcb657612674f31fdb2ec9c5f37cc978ca2e
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-10-02 11:56:30 -0700

    Update TclParse.cpp

--- SRC/runtime/parsing/TclParse.cpp
+++ SRC/runtime/parsing/TclParse.cpp
@@ -53,3 +53,10 @@ Tcl_SplitList(Tcl_Interp *interp, const char *list, int *argcPtr, char ***argvPt
     }
     return TCL_OK;
 }
+
+int
+Tcl_Free(char *ptr)
+{
+    delete[] ptr;
+    return TCL_OK;
+}
