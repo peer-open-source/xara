646937ea71556e631c15d98b30f69401aba22313
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-02-27 20:32:29 -0800

    Add J2BeamThread3d material (J2 plasticity)
    Introduce J2BeamThread3d NDMaterial (SRC/material/plastic/J2BeamThread3d.{cpp,h}). Implements a 3-component J2 plasticity beam/fiber material with isotropic and kinematic hardening, return-mapping via Newton solve, consistent tangent computation, stress evaluation, parameter handling, sensitivity support, send/recv, and standard commit/revert methods. Provides thread-safe lightweight Vector/Matrix wrappers and JSON/print output. Useful for beam/fiber formulations where transverse stress components are constrained.

---
+++ SRC/material/plastic/J2BeamThread3d.h
@@ -0,0 +1,111 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 1999, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+//
+//
+#ifndef J2BeamThread3d_h
+#define J2BeamThread3d_h
+
+#define ND_TAG_J2BeamThread3d 92516
+
+#include <NDMaterial.h>
+
+#include <Matrix.h>
+#include <Vector.h>
+#include <ID.h>
+#include <Information.h>
+#include <Parameter.h>
+#include <Matrix3D.h>
+#include <VectorND.h>
+#include <Vector3D.h>
+
+
+namespace OpenSees {
+
+class J2BeamThread3d : public NDMaterial {
+public:
+  J2BeamThread3d(int tag, double E, double nu, double sigY, double Hi, double Hk);
+  J2BeamThread3d();
+  ~J2BeamThread3d();
+
+  const char* getClassType() const override {return "J2BeamThread3d";}
+
+  int setTrialStrain(const Vector&) override;
+  int setTrialStrainIncr(const Vector& v) override;
+  const Matrix& getTangent() override;
+  const Matrix& getInitialTangent() override;
+  const Vector& getStress() override;
+  const Vector& getStrain() override;
+  bool threadSafe() const override {return true;}
+  Vector3D getFrameStress();
+  OpenSees::Matrix3D getFrameTangent();
+
+
+  int commitState() override;
+  int revertToLastCommit() override;
+  int revertToStart() override;
+
+  NDMaterial* getCopy() override;
+  NDMaterial* getCopy(const char* type) override;
+  const char* getType() const override;
+  int getOrder() const;
+
+  int sendSelf(int commitTag, Channel& ) override;
+  int recvSelf(int commitTag, Channel& , FEM_ObjectBroker&) override;
+
+  void Print(OPS_Stream& s, int flag) override;
+
+  int setParameter(const char** argv, int argc, Parameter&) override;
+  int updateParameter(int parameterID, Information&) override;
+  int activateParameter(int paramID);
+
+  const Vector& getStressSensitivity(int gradIndex, bool conditional);
+  int commitSensitivity(const Vector& depsdh, int gradIndex, int numGrads);
+
+
+private:
+  double E;
+  double nu;
+  double sigmaY;
+  double Hiso;
+  double Hkin;
+
+  int parameterID;
+  Matrix* SHVs;
+
+  Matrix3D D;     // tangent
+  Vector3D Tepsilon;     // Trial strains
+  Vector3D sigma;
+  Vector s_wrapper;
+  Vector e_wrapper;
+  Matrix D_wrapper;
+
+
+  double alphan;
+  double alphan1;
+
+  double epsPn[3];
+  double epsPn1[3];
+  // Vector3D xsi;
+
+  double dg_n1;
+};
+} // namespace OpenSees
+
+#endif
