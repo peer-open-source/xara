70583b21bad974898b3f1ab71b3d1e6ac9d5fc51
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-03-04 10:56:22 -0800

    Update ElastomericBearingBoucWenMod3d.cpp

--- SRC/element/Bearing/elastomeric/ElastomericBearingBoucWenMod3d.cpp
+++ SRC/element/Bearing/elastomeric/ElastomericBearingBoucWenMod3d.cpp
@@ -18,7 +18,6 @@
 #include <Node.h>
 #include <Channel.h>
 #include <FEM_ObjectBroker.h>
-#include <Renderer.h>
 #include <Information.h>
 #include <ElementResponse.h>
 #include <UniaxialMaterial.h>
@@ -29,7 +28,6 @@
 #include <string.h>
 
 #include <OPS_Globals.h>
-#include <Message.h>
 
 #include <iostream>
 
@@ -872,18 +870,6 @@ int ElastomericBearingBoucWenMod3d::recvSelf(int commitTag, Channel &rChannel,
     return -1;
 }
 
-
-int ElastomericBearingBoucWenMod3d::displaySelf(Renderer &theViewer,
-    int displayMode, float fact)
-{
-    static Vector v1(3);
-    static Vector v2(3);
-
-    theNodes[0]->getDisplayCrds(v1, fact, displayMode);
-    theNodes[1]->getDisplayCrds(v2, fact, displayMode);
-
-    return theViewer.drawLine(v1, v2, 1.0, 1.0, this->getTag());
-}
 
 
 void ElastomericBearingBoucWenMod3d::Print(OPS_Stream &s, int flag)
