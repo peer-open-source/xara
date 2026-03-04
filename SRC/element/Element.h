f3ca8dffbe7783ff877ea768a0600bc56854251a
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-03-04 09:25:21 -0800

    Update Element.h

--- SRC/element/Element.h
+++ SRC/element/Element.h
@@ -40,12 +40,22 @@ class Info;
 class Response;
 class ElementalLoad;
 class Node;
+class FE_Element;
 
 class Element : public TaggedObject, public MovableObject
 {
   public:
     Element(int tag, int classTag);    
     virtual ~Element();
+    enum class Status {
+        FE_NotImplemented = -2,
+        Failed = -1,
+        Success = 0,
+    };
+    enum class MassType {
+        Translation,
+        General
+    };
 
     // methods dealing with nodes and number of external dof
 
@@ -71,21 +81,26 @@ class Element : public TaggedObject, public MovableObject
     virtual int  revertToLastCommit() = 0;
     virtual int  revertToStart();
     virtual int  update();
+    // virtual Status update(FE_Element&) {return Status::FE_NotImplemented;}
     virtual bool isSubdomain();
     
     // methods to return the current linearized stiffness,
     // damping and mass matrices
+    // virtual int getTangent(FE_Element&) {return -1;}
     virtual const Matrix &getTangentStiff() =0;
     virtual const Matrix &getInitialStiff() =0;
     virtual const Matrix &getDamp();
     virtual const Matrix &getMass();
+    virtual MassType getMassType() const {return MassType::General;}
+    virtual double getPositionInertia(int node) const {return 0.0;}
+    
 
     // methods for applying loads
     virtual void zeroLoad();	
     virtual int  addLoad(ElementalLoad *, double loadFactor);
     virtual int  addLoad(ElementalLoad *, const Vector &loadFactors);
 
-    virtual int addInertiaLoadToUnbalance(const Vector &accel);
+    // virtual int addInertiaLoadToUnbalance(const Vector &accel);
     virtual int setRayleighDampingFactors(double alphaM, double betaK, double betaK0, double betaKc);
 
     // methods for obtaining resisting force (force includes elemental loads)
@@ -123,9 +138,20 @@ class Element : public TaggedObject, public MovableObject
         this->domain = nullptr;
         return 0;
     }
+
+    int link(FE_Element& fe) {
+        m_FE = &fe;
+        return 0;
+    }
+    int unlink(FE_Element& fe) {
+        if (m_FE == &fe)
+            m_FE = nullptr;
+        return 0;
+    }
     Domain* getDomain() const {
       return this->domain;
     }
+
 protected:
     const Vector& getRayleighDampingForces();
 
@@ -139,6 +165,7 @@ protected:
 private:
 //  std::vector<Node*> nodes;
     bool is_this_element_active;
+    FE_Element* m_FE;
 
     int index, nodeIndex;
     static Matrix ** theMatrices; 
