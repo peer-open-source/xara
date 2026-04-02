89715e4aa3a095e643d7b920d4f265e5df2a76a6
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-04-02 14:20:30 -0700

    Update ZeroLengthInterface2D.h

--- SRC/element/Point/ZeroLengthInterface2D.h
+++ SRC/element/Point/ZeroLengthInterface2D.h
@@ -82,12 +82,8 @@
 #include <Element.h>
 #include <Matrix.h>
 
-// Tolerance for zero length of element
-#define	LENTOL 1.0e-6
-
 class Node;
 class Channel;
-//class UniaxialMaterial;
 class Response;
 
 class ZeroLengthInterface2D: public Element
@@ -102,38 +98,36 @@ class ZeroLengthInterface2D: public Element
   ~ZeroLengthInterface2D();
 
   // public methods to obtain information about dof & connectivity
-  int getNumExternalNodes(void) const;
-  const ID &getExternalNodes(void);
-  Node **getNodePtrs(void);
-  int getNumDOF(void);
-  void setDomain(Domain *theDomain);
+  int getNumExternalNodes() const;
+  const ID &getExternalNodes();
+  Node **getNodePtrs();
+  int getNumDOF();
+  void setDomain(Domain *);
 
   // public methods to set the state of the element
-  int commitState(void);
-  int revertToLastCommit(void);
-  int revertToStart(void);
-  int update(void);
+  int commitState();
+  int revertToLastCommit();
+  int revertToStart();
+  int update();
 
   // public methods to obtain stiffness, mass, damping and residual information
-  const Matrix &getTangentStiff(void);
-  const Matrix &getInitialStiff(void);
-  const Matrix &getDamp(void);
-  const Matrix &getMass(void);
-  void zeroLoad(void);
+  const Matrix &getTangentStiff();
+  const Matrix &getInitialStiff();
+  const Matrix &getDamp();
+  const Matrix &getMass();
+  void zeroLoad();
   int addLoad(ElementalLoad *theLoad, double loadFactor);
   int addInertiaLoadToUnbalance(const Vector &accel);
-  const Vector &getResistingForce(void);
-  const Vector &getResistingForceIncInertia(void);
+  const Vector &getResistingForce();
+  const Vector &getResistingForceIncInertia();
 
   // public methods for element output
   int sendSelf(int commitTag, Channel &theChannel);
   int recvSelf(int commitTag, Channel &theChannel, FEM_ObjectBroker &theBroker);
-  int displaySelf(Renderer &, int mode, float fact, const char **displayModes=0, int numModes=0);
-  void Print(OPS_Stream &s, int flag =0);
+  void Print(OPS_Stream &s, int flag);
   Response *setResponse(const char **argv, int argc, OPS_Stream &output);
   int getResponse(int responseID, Information &eleInformation);
 
-  //void updateDir (const Vector& x, const Vector& y);
 
  protected:
 
