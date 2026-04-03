1171eeca293b10b77b4fbc0984af6c3335f9f768
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-04-03 00:36:32 -0700

    Update EulerFrame3d.cpp

--- SRC/element/Frame/EulerFrame3d.cpp
+++ SRC/element/Frame/EulerFrame3d.cpp
@@ -2,7 +2,16 @@
 //
 //                                   xara
 //                              https://xara.so
-//----------------------------------------------------------------------------//
+//
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2025, OpenSees/Xara Developers
+// All rights reserved.  No warranty, explicit or implicit, is provided.
+//
+// This source code is licensed under the BSD 2-Clause License.
+// See LICENSE file or https://opensource.org/licenses/BSD-2-Clause
+//
+//===----------------------------------------------------------------------===//
 //
 // Linearized Euler frame formulation with C1 displacement interpolation
 //
