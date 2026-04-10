6bb687aa5195852631f958dfd9e5744963e978f1
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-04-10 06:37:40 -0700

    Update KikuchiBearing.cpp

--- SRC/element/community/HUelements/KikuchiBearing.cpp
+++ SRC/element/community/HUelements/KikuchiBearing.cpp
@@ -66,441 +66,6 @@
 extern "C" double dbesi0(double);
 extern "C" double dbesi1(double);
 
-#if 0
-#include <elementAPI.h>
-static bool errDetected(bool ifNoError, const char *msg){
-  if (ifNoError){
-    opserr << "" << endln;
-    opserr << "========================================" << endln;
-    opserr << "KikuchiBearing element : input error detected" << endln;
-    opserr << "------------------------------" << endln;
-  }
-  opserr << "  " << msg << endln;
-  return false;
-};
-
-
-void * OPS_ADD_RUNTIME_VPV(OPS_KikuchiBearing)
-{
-
-    //3-dim, 6dof
-    int ndm = OPS_GetNDM();
-    int ndf = OPS_GetNDF();
-
-    if (ndm != 3 || ndf != 6) {
-    	opserr << "ndm=" << ndm << ", ndf=" << ndf << endln;
-    	opserr << "WARNING KikuchiBearing command only works when ndm is 3 and ndf is 6" << endln;
-    	return 0;
-    }
-
-    //arguments (necessary)
-    int eleTag;
-    int iNode;
-    int jNode;
-
-    //arguments (necessary, input with -???)
-    int shape;
-    double size;
-    double totalRubber;
-    int nMSS;
-    int matMSSTag;
-    UniaxialMaterial *matMSS;
-    int nMNS;
-    int matMNSTag;
-    UniaxialMaterial *matMNS;
-
-    //arguments (optional, input with -???)
-    double totalHeight = -1.0; //default: Norm(I->J)
-    double limDisp = -1.0; //default: INF
-    double lambda = -1.0; //default: INF
-    Vector oriX(0); //default: local-x Vec(I->J)
-    Vector oriYp(3); oriYp(0) = 0.0; oriYp(1) = 1.0; oriYp(2) = 0.0; //default: global-Y
-    double mass = 0.0;
-    bool ifPDInput = true;
-    bool ifTilt = true;
-    double adjCi = 0.5;
-    double adjCj = 0.5;
-    bool ifBalance = false;
-    double limFo = -1.0; //default: INF
-    double limFi = -1.0; //default: INF
-    int nIter = 1;
-
-    // input comfirmation
-    int recvShape  = 0;
-    int recvSize   = 0;
-    int recvHeight = 0;
-    int recvNMSS  = 0;
-    int recvMatMSS  = 0;
-    int recvLimDisp = 0;
-    int recvNMNS  = 0;
-    int recvMatMNS  = 0;
-    int recvLambda = 0;
-    int recvOrient = 0;
-    int recvMass   = 0;
-    int recvIfPD = 0;
-    int recvIfTl = 0;
-    int recvAdj = 0;
-    int recvBal = 0;
-
-    //
-    Element *theElement = 0;
-
-
-    //error flag
-    bool ifNoError = true;
-
-
-
-    if (OPS_GetNumRemainingInputArgs() < 3)  { //element KikuchiBearing eleTag? iNode? jNode?
-
-	ifNoError = errDetected(ifNoError,"insufficient arguments");
-
-    } else {
-
-    	//argv[2~4]
-    	int numdata = 1;
-    	if (OPS_GetIntInput(&numdata, &eleTag) < 0) {
-    	    ifNoError = errDetected(ifNoError,"invalid eleTag");
-    	}
-
-    	if (OPS_GetIntInput(&numdata, &iNode) < 0) {
-    	    ifNoError = errDetected(ifNoError,"invalid iNode");
-    	}
-
-    	if (OPS_GetIntInput(&numdata, &jNode) < 0) {
-    	    ifNoError = errDetected(ifNoError,"invalid jNode");
-    	}
-    
-	//argv[5~]
-    	while (OPS_GetNumRemainingInputArgs() > 0) {
-      
-	    double value;
-     	    const char* flag = OPS_GetString();
-
-	    if (strcmp(flag,"-shape")==0 && OPS_GetNumRemainingInputArgs()>0) { // -shape shape?
-    		const char* shapeflag = OPS_GetString();
-    		if (strcmp(shapeflag,"round") == 0) {
-    		    shape = 1; //round
-    		} else if (strcmp(shapeflag,"square") == 0) {
-    		    shape = 2; //square
-    		} else {
-    		    ifNoError = errDetected(ifNoError,"invalid shape (\"round\" or \"square\" are available)");
-    		}
-	
-    		recvShape++ ;
-
-
-    	    } else if (strcmp(flag,"-size")==0 && OPS_GetNumRemainingInputArgs()>1) { // -size size? totalRubber?
-
-    		numdata = 1;
-    		if (OPS_GetDoubleInput(&numdata, &size)<0 || size<=0) {
-    		    ifNoError = errDetected(ifNoError,"invalid size");
-    		}
-
-    		if (OPS_GetDoubleInput(&numdata, &totalRubber)<0 || totalRubber<=0) {
-    		    ifNoError = errDetected(ifNoError,"invalid totalRubber");
-    		}
-
-    		recvSize++ ;
-	
-    	    } else if (strcmp(flag,"-totalHeight")==0 && OPS_GetNumRemainingInputArgs()>0) { // -totalHeight totalHeight?
-
-    		numdata = 1;
-    		if (OPS_GetDoubleInput(&numdata, &totalHeight)<0 || totalHeight<=0) {
-    		    ifNoError = errDetected(ifNoError,"invalid totalHeight");
-    		}
-
-    		recvHeight++ ;
-
-    	    } else if (strcmp(flag,"-nMSS")==0 && OPS_GetNumRemainingInputArgs()>0) { // -nMSS nMSS?
-
-    		numdata = 1;
-    		if (OPS_GetIntInput(&numdata, &nMSS)<0 || nMSS<=0) {
-    		    ifNoError = errDetected(ifNoError,"invalid nMSS");
-    		}
-
-    		recvNMSS++ ;
-
-
-    	    } else if (strcmp(flag,"-matMSS")==0 && OPS_GetNumRemainingInputArgs()>0) { // -matMSS matMSSTag?
-
-    		numdata = 1;
-    		if (OPS_GetIntInput(&numdata, &matMSSTag)<0) {
-    		    ifNoError = errDetected(ifNoError,"invalid matMSSTag");
-    		}
-
-    		matMSS = OPS_getUniaxialMaterial(matMSSTag);
-    		if (matMSS == 0)  {
-    		    ifNoError = errDetected(ifNoError,"material for MSS model not found");
-    		}
-
-    		recvMatMSS++ ;
-
-
-    	    } else if (strcmp(flag,"-limDisp")==0 && OPS_GetNumRemainingInputArgs()>0) { // <-limDisp limDisp?>
-
-    		numdata = 1;
-    		if (OPS_GetDoubleInput(&numdata, &limDisp)<0 || limDisp<0) {
-    		    ifNoError = errDetected(ifNoError,"invalid limDisp");
-    		}
-
-    		recvLimDisp++ ;
-
-
-    	    } else if (strcmp(flag,"-nMNS")==0 && OPS_GetNumRemainingInputArgs()>0) { // -nMNS nMNS?
-
-    		numdata = 1;
-    		if (OPS_GetIntInput(&numdata, &nMNS)<0 || nMNS<=0) {
-    		    ifNoError = errDetected(ifNoError,"invalid nMNS");
-    		}
-
-    		recvNMNS++ ;
-
-
-    	    } else if (strcmp(flag,"-matMNS")==0 && OPS_GetNumRemainingInputArgs()>0) { // -matMNS matMNSTag?
-    		numdata = 1;
-    		if (OPS_GetIntInput(&numdata, &matMNSTag)<0) {
-    		    ifNoError = errDetected(ifNoError,"invalid matMNSTag");
-    		}
-
-    		matMNS = OPS_getUniaxialMaterial(matMNSTag);
-    		if (matMNS == 0)  {
-    		    ifNoError = errDetected(ifNoError,"material for MNS model not found");
-    		}
-
-    		recvMatMNS++ ;
-
-
-    	    } else if (strcmp(flag,"-lambda")==0 && OPS_GetNumRemainingInputArgs()>0) { // <-lambda lambda?>
-    		numdata = 1;
-    		if (OPS_GetDoubleInput(&numdata, &lambda)<0 || lambda<0) {
-    		    ifNoError = errDetected(ifNoError,"invalid lambda");
-    		}
-
-    		recvLambda++ ;
-
-
-    	    } else if (strcmp(flag,"-orient")==0 && OPS_GetNumRemainingInputArgs()>=6) { // <-orient x1? x2? x3? yp1? yp2? yp3?>
-
-    		oriX.resize(3);
-
-    		for (int j=1; j<=3; j++) {
-    		    numdata = 1;
-    		    if (OPS_GetDoubleInput(&numdata, &value)<0) {
-    			ifNoError = errDetected(ifNoError,"invalid orient");
-    		    } else {
-    			oriX(j-1) = value;
-    		    }
-    		}
-
-    		for (int j=1; j<=3; j++) {
-    		    numdata = 1;
-    		    if (OPS_GetDoubleInput(&numdata, &value)<0) {
-    			ifNoError = errDetected(ifNoError,"invalid orient");
-    		    } else {
-    			oriYp(j-1) = value;
-    		    }
-    		}
-
-    		recvOrient++ ;
-
-    	    } else if (strcmp(flag,"-orient")==0 && OPS_GetNumRemainingInputArgs()>=3) { // <-orient yp1? yp2? yp3?>
-
-    		for (int j=1; j<=3; j++) {
-    		    numdata = 1;
-    		    if (OPS_GetDoubleInput(&numdata, &value)<0) {
-    			ifNoError = errDetected(ifNoError,"invalid orient");
-    		    } else {
-    			oriYp(j-1) = value;
-    		    }
-    		}
-
-    		recvOrient++ ;
-
-    	    } else if (strcmp(flag,"-mass")==0 && OPS_GetNumRemainingInputArgs()>0) { // <-mass mass?>
-
-    		numdata = 1;
-    		if (OPS_GetDoubleInput(&numdata, &mass)<0 || mass<=0) {
-    		    ifNoError = errDetected(ifNoError,"invalid mass");
-    		}
-
-    		recvMass++ ;
-
-    	    } else if (strcmp(flag,"-noPDInput")==0) { // <-noPDInput>
-	
-    		ifPDInput = false;
-
-    		recvIfPD++ ;
-
-    	    } else if (strcmp(flag,"-noTilt")==0) { // <-noTilt>
-	
-    		ifTilt = false;
-
-    		recvIfTl++ ;
-	
-    	    } else if (strcmp(flag,"-adjustPDOutput")==0 && OPS_GetNumRemainingInputArgs()>1) { // -adjustPDOutput ci? cj?
-
-    		numdata = 1;
-    		if (OPS_GetDoubleInput(&numdata, &adjCi)<0 || adjCi<=0) {
-    		    ifNoError = errDetected(ifNoError,"invalid ci");
-    		}
-
-    		if (OPS_GetDoubleInput(&numdata, &adjCj)<0 || adjCj<=0) {
-    		    ifNoError = errDetected(ifNoError,"invalid cj");
-    		}
-
-    		recvAdj++ ;
-
-    	    } else if (strcmp(flag,"-doBalance")==0 && OPS_GetNumRemainingInputArgs()>2) { // -doBalance limFo? limFi? nIter?
-
-    		numdata = 1;
-    		if (OPS_GetDoubleInput(&numdata, &limFo)<0 || limFo<=0) {
-    		    ifNoError = errDetected(ifNoError,"invalid limFo");
-    		}
-
-    		if (OPS_GetDoubleInput(&numdata, &limFi)<0 || limFi<=0) {
-    		    ifNoError = errDetected(ifNoError,"invalid limFi");
-    		}
-
-    		if (OPS_GetIntInput(&numdata, &nIter)<0 || nIter<=0) {
-    		    ifNoError = errDetected(ifNoError,"invalid nIter");
-    		}
-
-    		ifBalance = true;
-
-    		recvBal++ ;
-
-
-    	    } else { //invalid option
-	
-    		ifNoError = errDetected(ifNoError,"invalid optional arguments");
-    		break;
-
-    	    }
-
-    	}
-    
-    } //end input
-  
-
-    // input cofirmation
-    // necessary arguments
-    if (recvShape != 1)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -shape inputs (got %d inputs, but want 1 input)",recvShape);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvSize != 1)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -size inputs (got %d inputs, but want 1 input)",recvSize);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvNMSS != 1)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -NMSS inputs (got %d inputs, but want 1 input)",recvNMSS);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvMatMSS != 1)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -matMSS inputs (got %d inputs, but want 1 input)",recvMatMSS);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvNMNS != 1)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -NMNS inputs (got %d inputs, but want 1 input)",recvNMNS);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvMatMNS != 1)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -matMNS inputs (got %d inputs, but want 1 input)",recvMatMNS);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-
-    //optional arguments
-    if (recvHeight >= 2)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -totalHeight inputs (got %d inputs, but want 1 input)",recvHeight);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvLimDisp >= 2)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -limDisp inputs (got %d inputs, but want 1 input)",recvLimDisp);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvLambda >= 2)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -lambda inputs (got %d inputs, but want 1 input)",recvLambda);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvOrient >= 2)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -ori inputs (got %d inputs, but want 1 input)",recvOrient);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvMass >= 2)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -mass inputs (got %d inputs, but want 1 input)",recvMass);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvIfPD >= 2)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -noPDInput inputs (got %d inputs, but want 1 input)",recvIfPD);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvIfTl >= 2)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -noTilt inputs (got %d inputs, but want 1 input)",recvIfTl);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvAdj >= 2)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -adjustPDOutput inputs (got %d inputs, but want 1 input)",recvAdj);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-    if (recvBal >= 2)  {
-    	char buf[100];
-    	sprintf(buf,"wrong number of -doBalance inputs (got %d inputs, but want 1 input)",recvBal);
-    	ifNoError = errDetected(ifNoError,buf);
-    }
-
-  
-    //if error detected
-    if (!ifNoError) {
-    	opserr << "------------------------------" << endln;
-    	//input:
-    	//printCommand(argc, argv);
-    	//want:
-    	opserr << "Want: element KikuchiBearing eleTag? iNode? jNode?\n";
-    	opserr << "                             -shape shape? -size size? totalRubber? <-totalHeight totalHeight?>\n";
-    	opserr << "                             -nMSS nMSS? -matMSS matMSSTag? <-lim limDisp?>\n";
-    	opserr << "                             -nMNS nMNS? -matMNS matMNSTag? <-lambda lambda?>\n";
-    	opserr << "                             <-orient <x1? x2? x3?> yp1? yp2? yp3?> <-mass m?>\n";
-    	opserr << "                             <-noPDInput> <-noTilt> <-adjustPDOutput ci? cj?> <-doBalance limFo? limFi? nIter?>\n";
-    	opserr << "========================================" << endln;
-    	opserr << "" << endln;
-    	return 0;
-    }
-
-    // now create the KikuchiBearing
-    theElement = new KikuchiBearing(eleTag, iNode, jNode, shape, size, totalRubber, totalHeight, nMSS, matMSS, limDisp, nMNS, matMNS, lambda, oriYp, oriX, mass, ifPDInput, ifTilt, adjCi, adjCj, ifBalance, limFo, limFi, nIter);
-    return theElement;
-
-}
-#endif
 
 // initialize the class wide variables
 Matrix KikuchiBearing::theMatrix(12,12);
@@ -532,17 +97,17 @@ Vector KikuchiBearing::dspCpnt(9);
 
 
 KikuchiBearing::KikuchiBearing(int Tag, int Nd1, int Nd2,
-			       int Shape, double Size, double TotalRubber, double TotalHeight,
-			       int NMSS, 
-             UniaxialMaterial &MatMSS, 
-             double LimDisp,
-			       int NMNS, 
-             UniaxialMaterial &MatMNS, 
-             double Lambda,
-			       const Vector OriYp, const Vector OriX, double Mass,
-			       bool IfPDInput, bool IfTilt,
-			       double AdjCi, double AdjCj,
-			       bool IfBalance, double LimFo, double LimFi, int NIter)
+                                int Shape, double Size, double TotalRubber, double TotalHeight,
+                                int NMSS, 
+                                UniaxialMaterial &MatMSS, 
+                                double LimDisp,
+                                int NMNS, 
+                                UniaxialMaterial &MatMNS, 
+                                double Lambda,
+                                const Vector OriYp, const Vector OriX, double Mass,
+                                bool IfPDInput, bool IfTilt,
+                                double AdjCi, double AdjCj,
+                                bool IfBalance, double LimFo, double LimFi, int NIter)
   : Element(Tag, ELE_TAG_KikuchiBearing),
     connectedExternalNodes(2),
     shape(Shape),size(Size),totalRubber(TotalRubber), totalHeight(TotalHeight),
@@ -595,14 +160,14 @@ KikuchiBearing::KikuchiBearing(int Tag, int Nd1, int Nd2,
   posLz = new double [nMNS*nMNS]; //local-z position
   distFct = new double [nMNS*nMNS]; //distribution factor
 
-  if (shape == 1) {//round shape
-    incA = (M_PI*size*size)/(4.0*nMNS*nMNS); //area of each normal spring
+  if (shape == 1) {// round shape
+    incA = (M_PI*size*size)/(4.0*nMNS*nMNS); // area of each normal spring
 
     int p = nMNS%2;
     int k = -1; //index
   
     for (int i=1; i<=((nMNS+p)/2); i++) {
-      //circle
+      // circle
       if (i==1 && p==1) {
         k++; //k=0
         double r = 0.0;
@@ -620,7 +185,7 @@ KikuchiBearing::KikuchiBearing(int Tag, int Nd1, int Nd2,
         continue;
       }
 
-      //circular sector
+      // circular sector
       double tht = M_PI/(4.0*(2*i-1-p));
       double r1 = ((2.0*i-2-p)/(2.0*nMNS))*size;
       double r2 = ((2.0*i-p)/(2.0*nMNS))*size;
@@ -672,7 +237,7 @@ KikuchiBearing::KikuchiBearing(int Tag, int Nd1, int Nd2,
     }
   }
 
-  //ave(distFct) = 1.0
+  // ave(distFct) = 1.0
   double aveDistFct = 0.0;
   for(int k=0; k<nMNS*nMNS; k++) {
     aveDistFct += distFct[k];//sumDistFct
@@ -702,14 +267,14 @@ KikuchiBearing::KikuchiBearing(int Tag, int Nd1, int Nd2,
     sinTht[i] = sin(M_PI*i/nMSS);
   }
 
-  //commitDisp for MSS
+  // commitDisp for MSS
   commitDspMss = new double [nMSS];
   for (int i=0; i<nMSS; i++)  {
     commitDspMss[i] = 0.0;
   }
 
 
-  //stiff springs in mid-height
+  // stiff springs in mid-height
   double sect_aa = 0.0; //area
   double sect_ii = 0.0; //moment of inertia of area
   double sect_pp = 0.0; //polar moment of inertia of area
@@ -727,7 +292,7 @@ KikuchiBearing::KikuchiBearing(int Tag, int Nd1, int Nd2,
   stfMidX  = ((theINodeMNSMaterials[0]->getInitialTangent()) * sect_aa/(totalRubber)) * 1e4;
   stfMidRY = ((theINodeMNSMaterials[0]->getInitialTangent()) * sect_ii/(totalRubber)) * 1e4;
   stfMidRZ = ((theINodeMNSMaterials[0]->getInitialTangent()) * sect_ii/(totalRubber)) * 1e4;
-  stfMidRX = ((theMidMSSMaterials[0]->getInitialTangent()) * sect_pp/(totalRubber)) * 1e4;
+  stfMidRX = ((  theMidMSSMaterials[0]->getInitialTangent()) * sect_pp/(totalRubber)) * 1e4;
 
   // initialize variables
   this->revertToStart();
@@ -757,7 +322,7 @@ KikuchiBearing::KikuchiBearing()
   
   // set node pointers to NULL
   for (int i=0; i<2; i++)
-    theNodes[i] = 0;
+    theNodes[i] = nullptr;
 
   // set material array pointers to NULL
   theINodeMNSMaterials = 0;
