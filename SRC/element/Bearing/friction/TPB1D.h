29a24135e23229d25fdc7dbc45aba941587bc346
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-06-05 19:02:54 -0700

    Update TPB1D.h

--- SRC/element/Bearing/friction/TPB1D.h
+++ SRC/element/Bearing/friction/TPB1D.h
@@ -95,10 +95,9 @@ class TPB1D : public Element
   const Vector &getResistingForceIncInertia(void);            
   
   // public methods for element output
-  int sendSelf(int commitTag, Channel &theChannel);
-  int recvSelf(int commitTag, Channel &theChannel, FEM_ObjectBroker &theBroker);
-  void Print(OPS_Stream &s, int flag =0);    
-  int displaySelf(Renderer &theViewer, int displayMode, float fact, const char **modes, int numMode);
+  int sendSelf(int commitTag, Channel &);
+  int recvSelf(int commitTag, Channel &, FEM_ObjectBroker &);
+  void Print(OPS_Stream &s, int flag);    
   
   Response *setResponse(const char **argv, int argc, OPS_Stream &s);
   int getResponse(int responseID, Information &eleInformation);
@@ -106,7 +105,7 @@ class TPB1D : public Element
   int setParameter(const char **argv, int argc, Parameter &param);
   int updateParameter(int parameterID, Information &info);
   int activateParameter(int parameterID);
-  
+
   void updateDir (const Vector& x, const Vector& y);
   
  protected:
