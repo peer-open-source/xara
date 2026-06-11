1106bc396e785e2f3cbd889e8118687e17f23400
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-06-11 15:08:30 -0700

    add banner

--- SRC/quadrature/Plane/LegendreFixedQuadrilateral.h
+++ SRC/quadrature/Plane/LegendreFixedQuadrilateral.h
@@ -1,3 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+//                                   xara
+//                              https://xara.so
+//
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2025, OpenSees/Xara Developers
+// All rights reserved.  No warranty, explicit or implicit, is provided.
+//
+// This source code is licensed under the BSD 2-Clause License.
+// See LICENSE file or https://opensource.org/licenses/BSD-2-Clause
+//
+//===----------------------------------------------------------------------===//
+//
 #ifndef LegendreFixedQuadrilateral_H
 #define LegendreFixedQuadrilateral_H
 
@@ -15,6 +30,7 @@ struct LegendreFixedQuadrilateral<4> {
   
   constexpr static double wts[4] = {1.0, 1.0, 1.0, 1.0};
 };
+
 
 template <>
 struct LegendreFixedQuadrilateral<9> {
