b958c6651fffb6495259072b2ae8b7e20988dffc
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-06-11 18:47:57 -0700

    remove displaySelf

--- SRC/element/community/HUelements/MultipleNormalSpring.h
+++ SRC/element/community/HUelements/MultipleNormalSpring.h
@@ -58,14 +58,14 @@ class MultipleNormalSpring : public Element
   ~MultipleNormalSpring();
   
   // method to get class type
-  const char *getClassType() const {return "MultipleNormalSpring";};
+  const char *getClassType() const {return "MultipleNormalSpring";}
   
   // public methods to obtain information about dof & connectivity    
   int getNumExternalNodes() const;
   const ID &getExternalNodes();
   Node **getNodePtrs();
   int getNumDOF();
-  void setDomain(Domain *theDomain);
+  void setDomain(Domain *);
   
   // public methods to set the state of the element    
   int commitState();
@@ -86,11 +86,10 @@ class MultipleNormalSpring : public Element
   const Vector &getResistingForceIncInertia();
   
   // public methods for element output
-  int sendSelf(int commitTag, Channel &theChannel);
-  int recvSelf(int commitTag, Channel &theChannel, FEM_ObjectBroker &theBroker);
-  int displaySelf(Renderer &theViewer, int displayMode, float fact, const char **modes, int numMode);
+  int sendSelf(int commitTag, Channel &);
+  int recvSelf(int commitTag, Channel &, FEM_ObjectBroker &);
 
-  void Print(OPS_Stream &s, int flag = 0);    
+  void Print(OPS_Stream &s, int flag);    
   
   // public methods for element recorder
   Response *setResponse(const char **argv, int argc, OPS_Stream &s);
