3dbdb75e00061a620bc21646d070a7fcdd1a18f0
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-06-27 00:11:11 -0700

    Create response.cpp

---
+++ SRC/runtime/commands/modeling/response.cpp
@@ -0,0 +1,134 @@
+//===----------------------------------------------------------------------===//
+//
+//                                   xara
+//                              https://xara.so
+//
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2025, OpenSees/Xara Developers
+// All rights reserved.  No warranty, explicit or implicit, is provided.
+//
+// This source code is licensed under the BSD 2-Clause License.
+// See LICENSE file or https://opensource.org/licenses/BSD-2-Clause
+//
+//===----------------------------------------------------------------------===//
+//
+#include <Element.h>
+#include <Domain.h>
+#include <Parsing.h>
+#include <Logging.h>
+#include <string>
+#include <ModelRegistry.h>
+#include <DummyStream.h>
+#include <recorder/response/Response.h>
+#include <runtime/runtime/InterpreterResponse.h>
+#include <cstdint>   // uintptr_t
+
+
+static Response*
+setElementResponse(Domain& domain, int eleTag, const char **argv, int argc)
+{
+  Element *theEle = domain.getElement(eleTag);
+
+  if (theEle == nullptr)
+    return nullptr;
+
+  DummyStream dummy;
+  return theEle->setResponse(argv, argc, dummy);
+}
+
+
+int 
+setResponse(ClientData clientData, Tcl_Interp *interp, int argc,
+            TCL_Char** const argv)
+{
+  assert(clientData != nullptr);
+  ModelRegistry *theRegistry = (ModelRegistry*)clientData;
+  Domain *theDomain = theRegistry->getDomain();
+
+  if (argc < 3) {
+    opserr << OpenSees::PromptValueError << "want - setResponse eleTag? args...\n";
+    return TCL_ERROR;
+  }
+
+  int tag;
+  if (Tcl_GetInt(interp, argv[1], &tag) != TCL_OK) {
+    opserr << OpenSees::PromptValueError << "failed to read eleTag? \n";
+    return TCL_ERROR;
+  }
+
+  Response *theResponse = setElementResponse(*theDomain, tag, argv + 2, argc - 2);
+  if (theResponse == nullptr) {
+    opserr << OpenSees::PromptValueError << "could not set response for element " << tag << "\n";
+    return TCL_ERROR;
+  }
+
+  if (theResponse->getResponse() < 0) {
+    opserr << OpenSees::PromptValueError << "failed to get response" << "\n";
+    return TCL_ERROR;
+  }
+
+
+  Information &info = theResponse->getInformation();
+
+  InterpreterResponse* iresponse = new InterpreterResponse(theResponse, 
+                                                           InterpreterResponse::Type::Vector, 
+                                                           info.theVector.Size(), 
+                                                           &info.theVector(0));
+
+  int id = theRegistry->addResponse(iresponse);
+
+  uintptr_t addr = reinterpret_cast<uintptr_t>(&info.theVector(0));
+
+  Tcl_AppendResult(interp,
+                   std::to_string(id).c_str(),                       " ",
+                   std::to_string(info.theVector.Size()).c_str(),    " ",
+                   std::to_string(addr).c_str(),
+                   (char*)nullptr);
+  return TCL_OK;
+}
+
+
+int
+getResponse(ClientData clientData, Tcl_Interp *interp, int argc,
+            TCL_Char** const argv)
+{
+  assert(clientData != nullptr);
+  ModelRegistry *theRegistry = (ModelRegistry*)clientData;
+
+  if (argc < 2) {
+    opserr << OpenSees::PromptValueError << "want - getResponse responseID? \n";
+    return TCL_ERROR;
+  }
+
+  int id;
+  if (Tcl_GetInt(interp, argv[1], &id) != TCL_OK) {
+    opserr << OpenSees::PromptValueError << "getResponse responseID? - could not read responseID? \n";
+    return TCL_ERROR;
+  }
+
+  InterpreterResponse *theResponse = theRegistry->getResponse(id);
+  if (theResponse == nullptr) {
+    opserr << OpenSees::PromptValueError << "could not find response with ID " << id << "\n";
+    return TCL_ERROR;
+  }
+
+  if (theResponse->response->getResponse() < 0) {
+    opserr << OpenSees::PromptValueError << "failed to get response for ID " << id << "\n";
+    return TCL_ERROR;
+  }
+
+  // Check the integrity of the response data
+  Information &info = theResponse->response->getInformation();
+  if (info.theVector.Size() != theResponse->size) {
+    opserr << OpenSees::PromptValueError << "response size mismatch" << "\n";
+    return TCL_ERROR;
+  }
+
+  if (&info.theVector(0) != theResponse->data) {
+    opserr << OpenSees::PromptValueError << "response data mismatch" << "\n";
+    return TCL_ERROR;
+  }
+
+  return TCL_OK;
+}
