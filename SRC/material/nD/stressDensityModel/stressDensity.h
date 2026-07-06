54c0c94b11da762e28f500e63a697cc489ce804f
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-07-05 22:32:19 -0700

    Update stressDensity.h

--- SRC/material/nD/stressDensityModel/stressDensity.h
+++ SRC/material/nD/stressDensityModel/stressDensity.h
@@ -34,25 +34,6 @@
 #include <Vector.h>
 #include <ID.h>
 
-#ifdef _WIN32
-# define sdmuc_ SDMUC
-#endif
-
-extern "C" void sdmuc_(
-                       double strhs[],
-                       double strsg[],
-                       double props[],
-                       double stran[],
-                       const int *nmats,
-                       const int *nstrp,
-                       const int *istep,
-                       const int *iiter,
-                       const int *ielem,
-                       double strhs0[],
-                       double etahs[][3],
-                       double hdp[][3],
-                       double oths[]);
-
 class stressDensity : public NDMaterial
 {
   public:
