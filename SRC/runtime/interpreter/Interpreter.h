c5e26875b528975a1ca2d3cb592d7dd08ba4a4e1
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-07-13 11:14:18 -0700

    Create Interpreter.h

---
+++ SRC/runtime/interpreter/Interpreter.h
@@ -0,0 +1,24 @@
+//===----------------------------------------------------------------------===//
+//
+//                                   xara
+//                              https://xara.so
+//
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2025, OpenSees/Xara Developers
+// All rights reserved.  No warranty, explicit or implicit, is provided.
+//
+// This source code is licensed under the BSD 2-Clause License.
+// See LICENSE file or https://opensource.org/licenses/BSD-2-Clause
+//
+//===----------------------------------------------------------------------===//
+//
+#include <runtime/parsing/Parsing.h>
+// typedef const char** const ArgList;
+
+namespace OpenSees {
+typedef Tcl_Interp Interpreter;
+}
+
+// template <typename T>
+// typedef int (*StrCmd)(T&, Interpreter&, ArgSize, ArgList);
