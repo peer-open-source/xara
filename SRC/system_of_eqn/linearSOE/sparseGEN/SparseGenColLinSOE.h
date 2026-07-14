0fa5266e71895358388e94b34e1ff99b5d29cbe1
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-07-14 13:27:34 -0700

    Update SparseGenColLinSOE.h

--- SRC/system_of_eqn/linearSOE/sparseGEN/SparseGenColLinSOE.h
+++ SRC/system_of_eqn/linearSOE/sparseGEN/SparseGenColLinSOE.h
@@ -17,9 +17,7 @@
 **   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
 **                                                                    **
 ** ****************************************************************** */
-
-#ifndef SparseGenColLinSOE_h
-#define SparseGenColLinSOE_h
+#pragma once
 
 // Written: fmk
 // Created: 04/98
@@ -62,17 +60,14 @@ public:
   virtual void setX(int loc, double value);
   virtual void setX(const Vector& x);
 
-  virtual int sendSelf(int commitTag, Channel& theChannel);
-  virtual int recvSelf(int commitTag, Channel& theChannel, FEM_ObjectBroker& theBroker);
+  virtual int sendSelf(int commitTag, Channel&);
+  virtual int recvSelf(int commitTag, Channel&, FEM_ObjectBroker& theBroker);
 
 
-// #ifdef _PARALLEL_INTERPRETERS
   friend class SuperLU;
   friend class ThreadedSuperLU;
   friend class DistributedSuperLU;
-// #else
-//   friend class SuperLU;
-// #endif
+
 
 protected:
   int size;              // order of A
@@ -85,6 +80,3 @@ protected:
 
 private:
 };
-
-
-#endif
