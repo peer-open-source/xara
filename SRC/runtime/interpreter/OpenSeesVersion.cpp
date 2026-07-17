fc1606933bd684b306226277c81e06d38c88cd19
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-07-17 04:00:15 -0700

    Create OpenSeesVersion.cpp

---
+++ SRC/runtime/interpreter/OpenSeesVersion.cpp
@@ -0,0 +1,30 @@
+#include <tcl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Logging.h>
+#include "OpenSeesVersion.h"
+#include "Interpreter.h"
+
+namespace Xara {
+
+OpenSeesVersion 
+GetCompatibilityVersion(Tcl_Interp *interp)
+{
+  OpenSeesVersion version = OpenSeesVersion::XaraLatest;
+  const char *version_str = nullptr;
+  if (getenv("XARA_COMPATIBILITY_VERSION") != nullptr) {
+    version_str = getenv("XARA_COMPATIBILITY_VERSION");
+    if (strcmp(version_str, "O3") == 0)
+      version = OpenSeesVersion::O3;
+    else {
+      opserr << "WARNING: unknown compatibility version '" << version_str << "'.\n";
+    }
+    return version;
+  }
+
+  
+  // 
+  return version;
+}
+
+}
