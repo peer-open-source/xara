e69d59c031fc7ad872fa8d5e39aa892c67f4c4ea
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-08-11 09:21:51 -0700

    Update GeneralizedJ2.cpp

--- SRC/material/plastic/GeneralizedJ2.cpp
+++ SRC/material/plastic/GeneralizedJ2.cpp
@@ -15,16 +15,22 @@
 //
 // cmp
 //
-// Auricchio, F., and R.L. Taylor. 
-//   “Two Material Models for Cyclic Plasticity: Nonlinear Kinematic Hardening and Generalized Plasticity.” 
-//   International Journal of Plasticity 11, no. 1 (1995): 65–98. 
-//   https://doi.org/10.1016/0749-6419(94)00039-5.
+// References:
+//
+//  [1] Filippou, F.C. (1998)
+//     "FEDEASLab: Finite Elements for Design Evaluation and Analysis of Structures"
+//
+//  [2] Auricchio, F., and R.L. Taylor. 
+//      "Two Material Models for Cyclic Plasticity: Nonlinear Kinematic Hardening and Generalized Plasticity." 
+//      International Journal of Plasticity 11, no. 1 (1995): 65–98. 
+//      https://doi.org/10.1016/0749-6419(94)00039-5.
 //
 //
+// #include <matrix/routines/poly34.h>
 #include <GeneralizedJ2.h>
 #include <VectorND.h>
 #include <MatrixND.h>
-#include <Voight.hpp>
+#include <Voigt.hpp>
 #include <cstring>
 // #include <constmath/constmath.hpp>
 
@@ -45,7 +51,8 @@ static const double SQRT23 = std::sqrt(2.0/3.0);
 
 int
 GeneralizedJ2::hard_LP(const Hardening &hd, double yf_tr,
-                       double &Dlam, double &xi_p, double &Tlam) noexcept
+                       const TrialState &hs,
+                       double &Dlam, double &xi_p, double &theta_phi, double &Tlam) noexcept
 {
   const double H  = hd.Hi + hd.Hk;
   const double G  = hd.G;
@@ -53,16 +60,18 @@ GeneralizedJ2::hard_LP(const Hardening &hd, double yf_tr,
   Dlam = yf_tr / (2.0*G + H);
   if (Dlam < 0.0)
     Dlam = 0.0;
+
   xi_p = 2.0*G / (2.0*G + H);
   Tlam = 1.0;
+  theta_phi = Dlam/hs.sn;
   return 0;
 }
 
 
 int
 GeneralizedJ2::hard_GP(const Hardening &hd,
-                       double yf_tr, const HState &hs,
-                       double &Dlam, double &xi_p, double &Tlam) noexcept
+                       double yf_tr, const TrialState &hs,
+                       double &Dlam, double &xi_p, double &theta_phi, double &Tlam) noexcept
 {
   const double H     = hd.Hk + hd.Hi;
   const double G     = hd.G;
@@ -99,15 +108,33 @@ GeneralizedJ2::hard_GP(const Hardening &hd,
   if (x2 > 0.0 && x2 < Dlam) Dlam = x2;
   if (!std::isfinite(Dlam)) Dlam = 0.0;
 
-  // algorithmic tangent coefficient xi_p
+  // algorithmic tangent coefficient xi_p (Adiscr)
   // xi_p = TG*(B1 + B2) / ((TG + H)*B1 - (delta - 2.0*G)*B2 + B3)
   const double B1    = A2 + A3*Dlam;
   const double B2    = A1 - (2.0*G + H)*Dlam; 
   const double B3    = A4; 
   const double Denom = (2.0*G + H)*B1 - A3*B2 + B3;
+
+  // Adiscr
   xi_p  = (Denom != 0.0) ? (2.0*G*(B1 + B2)/Denom) : 1.0;
   Tlam  = 1.0;
+  theta_phi = Dlam/(hs.sn);
   return 0;
+}
+
+
+int
+GeneralizedJ2::hard_AF(const Hardening &hd,
+                       double yf_tr, 
+                       const TrialState &hs,
+                       double &Dlam, double &xi_p, 
+                       double & theta_phi,
+                       double &Tlam,
+                       VectorND<9> &M, // flow direction
+                       double & theta_nlk
+                      ) noexcept
+{
+  return -1;
 }
 
 //
@@ -168,11 +195,11 @@ GeneralizedJ2::updateState(const VectorND<6> &eps)
 
 
   // deviatoric part of total strain
-  const VectorND<6> eps_dev = Voight::Dev(eps);
-  const double      eps_vol = Voight::Trace(eps); // volumetric strain
+  const VectorND<6> eps_dev = Voigt::Dev(eps);
+  const double      eps_vol = Voigt::Trace(eps); // volumetric strain
 
   // Trial deviatoric stress: s_tr = 2G * (eps_dev - eps_p_d)
-  const VectorND<6> eps_p_d = Voight::Dev(past.eps_p);
+  const VectorND<6> eps_p_d = Voigt::Dev(past.eps_p);
   VectorND<9> sig_dev_tr{};
   sig_dev_tr.addVector(0.0, P^eps_dev,  2.0*G);
   sig_dev_tr.addVector(1.0, P^eps_p_d, -2.0*G);
@@ -191,9 +218,9 @@ GeneralizedJ2::updateState(const VectorND<6> &eps)
     // Full Cauchy stress = deviatoric + K*eps_v*IVOL
 
     // VectorND<6> sig = P*sig_dev_tr;
-    // sig.addVector(1.0, Voight::ivol, K*eps_vol);
-    pres.sig = Voight::ReduceVector(sig_dev_tr);
-    Voight::AddVol(pres.sig, K*eps_vol);
+    // sig.addVector(1.0, Voigt::ivol, K*eps_vol);
+    pres.sig = Voigt::ReduceVector(sig_dev_tr);
+    Voigt::AddVol(pres.sig, K*eps_vol);
 
     // pres.sig     = sig;
     // pres.eps     = eps;
@@ -204,8 +231,8 @@ GeneralizedJ2::updateState(const VectorND<6> &eps)
 
     // Tangent
     C.zero();
-    C.addMatrix(Voight::IoI,          K);
-    C.addMatrix(Voight::IIdevCon, 2.0*G);
+    C.addMatrix(Voigt::IoI,          K);
+    C.addMatrix(Voigt::IIdevCon, 2.0*G);
     return 0;
   }
 
@@ -222,27 +249,30 @@ GeneralizedJ2::updateState(const VectorND<6> &eps)
   // Hardening rule selection
   const double phi = Fs - Fy; // phi is distance to asymptote
   const Hardening hd{Hiso, Hkin, G, delta, phi, rule};
-  HState hs{};
+  TrialState hs{};
   hs.A2    = A2;
   hs.sn    = sig_nrm;
   hs.se    = sig_dev_tr;
   hs.sb    = past.sig_b;
   hs.alpha = past.alpha;
+  hs.flow_stress = (Fy + Hiso*past.alpha)*flow_rate;
 
   double Dlam=0.0, xi_p=1.0, Tlam=1.0;
+  double theta_phi, theta_nlk=0;
   int status = -1;
   switch (rule) {
-    case HRule::LP: status = hard_LP(hd, yf_tr, Dlam, xi_p, Tlam); break;
-    case HRule::GP: status = hard_GP(hd, yf_tr, hs, Dlam, xi_p, Tlam); break;
-    default:        status = hard_GP(hd, yf_tr, hs, Dlam, xi_p, Tlam); break;
+    case HRule::LP: status = hard_LP(hd, yf_tr, hs,          Dlam, xi_p, theta_phi, Tlam); break;
+    case HRule::GP: status = hard_GP(hd, yf_tr, hs,          Dlam, xi_p, theta_phi, Tlam); break;
+    case HRule::AF: status = hard_AF(hd, yf_tr, hs,          Dlam, xi_p, theta_phi, Tlam, n, theta_nlk); break;
+    default:        status = hard_GP(hd, yf_tr, hs,          Dlam, xi_p, theta_phi, Tlam); break;
   }
   if (status != 0) {
     opserr << "Hardening rule failed; falling back to elastic update\n";
     // Something went wrong in the hardening; fall back to elastic
     pres = past;
     C.zero();
-    C.addMatrix(Voight::IoI,          K);
-    C.addMatrix(Voight::IIdevCon, 2.0*G);
+    C.addMatrix(Voigt::IoI,          K);
+    C.addMatrix(Voigt::IIdevCon, 2.0*G);
     return -1;
   }
 
@@ -268,7 +298,7 @@ GeneralizedJ2::updateState(const VectorND<6> &eps)
 
   // Assemble full stress
   pres.sig = P*sig_dev;
-  pres.sig.addVector(1.0,  Voight::ivol,  K*eps_vol);
+  pres.sig.addVector(1.0,  Voigt::ivol,  K*eps_vol);
 
   // Store norm for next step
   pres.sigdnrm = (sig_dev - pres.sig_b).norm();
@@ -277,28 +307,44 @@ GeneralizedJ2::updateState(const VectorND<6> &eps)
   // Algorithmic tangent 
   //
   // (Auricchio–Taylor J2)
-  // Ct = K*IoI + TG*(1 - xi_p)*(n⊗n) + TG*(1 - TG*Dlam/sig_nrm)*(Id - n⊗n)
-  // Ct = K*IoI + TG*(1 - TG*Dlam/sig_nrm)*Id + TG*(1 - xi_p - (1 - TG*Dlam/sig_nrm))*(n⊗n)
-  //    = K*IoI + TG*(1 - TG*Dlam/sig_nrm)*Id + TG*(TG*Dlam/sig_nrm - xi_p)*n⊗n
+  // Ct = K*IoI + TG*(1 - xi_p)*(n o n) + TG*(1 - TG*Dlam/sig_nrm)*(Id - n o n)
+  // Ct = K*IoI + TG*(1 - TG*Dlam/sig_nrm)*Id + TG*(1 - xi_p - (1 - TG*Dlam/sig_nrm))*(n o n)
+  //    = K*IoI + TG*(1 - TG*Dlam/sig_nrm)*Id + TG*(TG*Dlam/sig_nrm - xi_p)*n o n
   //                      |       C      |               C             Ad
   C.zero();
-  C.addMatrix(Voight::IoI, K);
+  // 1. bulk term
+  C.addMatrix(Voigt::IoI, K);
 
   // IIdevCon := IIcon - IoI/3
-
+#if 1 // Original implementation
   const double a = 1.0 - TG*Dlam/sig_nrm;
   const double b = (1.0 - xi_p);
 
-  // term: TG * a * (Id - n⊗n)
+  // term: TG * a * (Id - n o n)
   MatrixND<6,6> nn{};
   nn.addTensorProduct(P*n,P*n, 1.0);
-  MatrixND<6,6> Pnn = Voight::IIdevCon;
+  MatrixND<6,6> Pnn = Voigt::IIdevCon;
   Pnn.addMatrix(nn, -1.0);
   C.addMatrix(Pnn, 2.0*G*a);
 
-  // term: TG * b * (n⊗n)
+  // term: TG * b * (n o n)
   C.addMatrix(nn, 2.0*G*b);
+#else
+  const double a = 1.0 - TG*Dlam/sig_nrm;
+  const double b = (1.0 - xi_p);
 
+  // 2. scale contravariant deviatoric identity
+  C.addMatrix(Voigt::IIdevCon,  2.0*G*(1.0 - 2.0*G*theta_phi));
+
+  const double theta_lam = xi_p;
+  const VectorND<6> Pn = P*n;
+  C.addTensorProduct(Pn, Pn,  -2.0*G*(theta_lam - 2.0*G*theta_phi));
+
+  if (theta_nlk != 0) {
+    // TODO
+  }
+
+#endif
   return 0;
 }
 
@@ -315,6 +361,7 @@ GeneralizedJ2::setTrialStrain(const Vector &v)
     eps[i] = v(i);
   return updateState(eps);
 }
+
 
 int
 GeneralizedJ2::setTrialStrainIncr(const Vector &dv)
@@ -381,8 +428,8 @@ GeneralizedJ2::revertToStart()
   const double K  = E/3./(1. - 2.*nu);     // Bulk  modulus
 
   Ce.zero();
-  Ce.addMatrix(Voight::IoI,          K);
-  Ce.addMatrix(Voight::IIdevCon, 2.0*G); // Idp
+  Ce.addMatrix(Voigt::IoI,          K);
+  Ce.addMatrix(Voigt::IIdevCon, 2.0*G); // Idp
   C = Ce;
   return 0;
 }
