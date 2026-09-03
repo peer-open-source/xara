47f1f70542a7fe7ef68c5c7c1578eb2a8b4bfc76
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-09-03 02:31:16 -0700

    Update ItpackLinSOE.cpp

--- SRC/system_of_eqn/linearSOE/itpack/ItpackLinSOE.cpp
+++ SRC/system_of_eqn/linearSOE/itpack/ItpackLinSOE.cpp
@@ -374,16 +374,6 @@ ItpackLinSOE::getB()
   return *vectB;
 }
 
-double 
-ItpackLinSOE::normRHS()
-{
-  double norm =0.0;
-  for (int i=0; i<size; i++) {
-    double Yi = B[i];
-    norm += Yi*Yi;
-  }
-  return sqrt(norm);
-}    
 
 int
 ItpackLinSOE::setItpackLinSolver(ItpackLinSolver &newSolver)
