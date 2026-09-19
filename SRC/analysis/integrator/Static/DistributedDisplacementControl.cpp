855f117122396c3bbcfa077484b2018477b43fdf
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-09-19 03:15:38 -0700

    Update DistributedDisplacementControl.cpp

--- SRC/analysis/integrator/Static/DistributedDisplacementControl.cpp
+++ SRC/analysis/integrator/Static/DistributedDisplacementControl.cpp
@@ -218,7 +218,6 @@ DistributedDisplacementControl::update(const Vector &dU)
     return -1;
   }
 
-  
   // set the X soln in linearSOE to be deltaU for convergence Test
   theLinSOE->setX(*deltaU);
 
@@ -232,155 +231,130 @@ DistributedDisplacementControl::update(const Vector &dU)
 int 
 DistributedDisplacementControl::domainChanged()
 {
-    // we first create the Vectors needed
-    AnalysisModel *theModel = this->getAnalysisModel();
-    LinearSOE *theLinSOE = this->getLinearSOE();    
-    if (theModel == 0 || theLinSOE == 0) {
-        opserr << "WARNING DistributedDisplacementControl::update() ";
-        opserr << "No AnalysisModel or LinearSOE has been set\n";
-        return -1;
-    }    
-    
-    //int size = theModel->getNumEqn(); // ask model in case N+1 space
-    const Vector &b = theLinSOE->getB(); // ask model in case N+1 space
-    int size = b.Size();
+  // we first create the Vectors needed
+  AnalysisModel *theModel = this->getAnalysisModel();
+  LinearSOE *theLinSOE = this->getLinearSOE();    
+  if (theModel == 0 || theLinSOE == 0) {
+      opserr << "WARNING DistributedDisplacementControl::update() ";
+      opserr << "No AnalysisModel or LinearSOE has been set\n";
+      return -1;
+  }    
+  
+  //int size = theModel->getNumEqn(); // ask model in case N+1 space
+  const Vector &b = theLinSOE->getB(); // ask model in case N+1 space
+  int size = b.Size();
 
-    // first we determine the id of the nodal dof
-    Domain *theDomain = theModel->getDomainPtr();
+  // first we determine the id of the nodal dof
+  Domain *theDomain = theModel->getDomainPtr();
 
-    theDofID = -1;
-    Node *theNodePtr = theDomain->getNode(theNode);
-    if (theNodePtr != 0) {
-      DOF_Group *theGroup = theNodePtr->getDOF_GroupPtr();
-      assert(theGroup != 0);
-      const ID &theID = theGroup->getID();
-      if (theDof < 0 || theDof >= theID.Size()) {
-        opserr << "DistributedDisplacementControl::domainChanged() - not a valid dof " << theDof << endln;
-        return -1;
-      }
-      theDofID = theID(theDof);
-      if (theDofID < 0) {
-        opserr << "DistributedDisplacementControl::domainChanged() - constrained dof not a valid a dof\n";;
-        return -1;
-      }
+  theDofID = -1;
+  Node *theNodePtr = theDomain->getNode(theNode);
+  if (theNodePtr != 0) {
+    DOF_Group *theGroup = theNodePtr->getDOF_GroupPtr();
+    assert(theGroup != 0);
+    const ID &theID = theGroup->getID();
+    if (theDof < 0 || theDof >= theID.Size()) {
+      opserr << "DistributedDisplacementControl::domainChanged() - not a valid dof " << theDof << endln;
+      return -1;
     }
+    theDofID = theID(theDof);
+    if (theDofID < 0) {
+      opserr << "DistributedDisplacementControl::domainChanged() - constrained dof not a valid a dof\n";;
+      return -1;
+    }
+  }
 
-    static ID data(1);    
+  static ID data(1);    
 
-    if (processID != 0) {
-      Channel *theChannel = theChannels[0];
-      data(0) = theDofID;
+  if (processID != 0) {
+    Channel *theChannel = theChannels[0];
+    data(0) = theDofID;
 
-      theChannel->sendID(0, 0, data);
+    theChannel->sendID(0, 0, data);
+    theChannel->recvID(0, 0, data);
+    theDofID = data(0);
+  } 
+
+  // if main domain, collect all theDofID if not -1 then this is the value & send value out
+  else {
+    
+    for (int j=0; j<numChannels; j++) {
+      Channel *theChannel = theChannels[j];
       theChannel->recvID(0, 0, data);
-      theDofID = data(0);
-    } 
-
-    // if main domain, collect all theDofID if not -1 then this is the value & send value out
-    else {
-      
-      for (int j=0; j<numChannels; j++) {
-        Channel *theChannel = theChannels[j];
-        theChannel->recvID(0, 0, data);
-        if (data(0) != -1)
-          theDofID = data(0);
-      }
-      
-      for (int k=0; k<numChannels; k++) {
-        Channel *theChannel = theChannels[k];
-        data(0) = theDofID;
-        theChannel->sendID(0, 0, data);
-      }
-    }
-
-    if (deltaUhat == 0 || deltaUhat->Size() != size) { // create new Vector
-        if (deltaUhat != 0)
-            delete deltaUhat;   // delete the old
-        deltaUhat = new Vector(size);
-        if (deltaUhat == 0 || deltaUhat->Size() != size) { // check got it
-            opserr << "FATAL DistributedDisplacementControl::domainChanged() - ran out of memory for";
-            opserr << " deltaUhat Vector of size " << size << endln;
-            exit(-1);
-        }
-    }
-
-    if (deltaUbar == 0 || deltaUbar->Size() != size) { // create new Vector
-        if (deltaUbar != 0)
-            delete deltaUbar;   // delete the old
-        deltaUbar = new Vector(size);
-        if (deltaUbar == 0 || deltaUbar->Size() != size) { // check got it
-            opserr << "FATAL DistributedDisplacementControl::domainChanged() - ran out of memory for";
-            opserr << " deltaUbar Vector of size " << size << endln;
-            exit(-1);
-        }
+      if (data(0) != -1)
+        theDofID = data(0);
     }
     
-    if (deltaU == 0 || deltaU->Size() != size) { // create new Vector
-        if (deltaU != 0)
-            delete deltaU;   // delete the old
-        deltaU = new Vector(size);
-        if (deltaU == 0 || deltaU->Size() != size) { // check got it
-            opserr << "FATAL DistributedDisplacementControl::domainChanged() - ran out of memory for";
-            opserr << " deltaU Vector of size " << size << endln;
-            exit(-1);
-        }
+    for (int k=0; k<numChannels; k++) {
+      Channel *theChannel = theChannels[k];
+      data(0) = theDofID;
+      theChannel->sendID(0, 0, data);
+    }
+  }
+
+  if (deltaUhat == 0 || deltaUhat->Size() != size) { // create new Vector
+    if (deltaUhat != 0)
+        delete deltaUhat;   // delete the old
+    deltaUhat = new Vector(size);
+  }
+
+  if (deltaUbar == 0 || deltaUbar->Size() != size) { // create new Vector
+    if (deltaUbar != 0)
+        delete deltaUbar;   // delete the old
+    deltaUbar = new Vector(size);
+  }
+  
+  if (deltaU == 0 || deltaU->Size() != size) { // create new Vector
+    if (deltaU != 0)
+      delete deltaU;   // delete the old
+    deltaU = new Vector(size);
+  }
+
+  if (deltaUstep == 0 || deltaUstep->Size() != size) { 
+      if (deltaUstep != 0)
+          delete deltaUstep;  
+      deltaUstep = new Vector(size);
+  }
+
+  if (phat == 0 || phat->Size() != size) { 
+    if (phat != 0)
+      delete phat;  
+    phat = new Vector(size);
+  }
+
+  // now we have to determine phat
+  // do this by incrementing lambda by 1, applying load
+  // and getting phat from unbalance.
+  currentLambda = theModel->getCurrentDomainTime();
+
+  currentLambda += 1.0;
+  theModel->applyLoadDomain(currentLambda);    
+  
+  this->formUnbalance(*phat); // NOTE: this assumes unbalance at last was 0
+
+  currentLambda -= 1.0;
+  theModel->setCurrentDomainTime(currentLambda);    
+
+  // check there is a reference load
+  int haveLoad = 0;
+  for (int i=0; i<size; i++)
+    if ( (*phat)(i) != 0.0 ) {
+      haveLoad = 1;
+      i = size;
     }
 
-    if (deltaUstep == 0 || deltaUstep->Size() != size) { 
-        if (deltaUstep != 0)
-            delete deltaUstep;  
-        deltaUstep = new Vector(size);
-        if (deltaUstep == 0 || deltaUstep->Size() != size) { 
-            opserr << "FATAL DistributedDisplacementControl::domainChanged() - ran out of memory for";
-            opserr << " deltaUstep Vector of size " << size << endln;
-            exit(-1);
-        }
-    }
-
-    if (phat == 0 || phat->Size() != size) { 
-        if (phat != 0)
-            delete phat;  
-        phat = new Vector(size);
-        if (phat == 0 || phat->Size() != size) { 
-            opserr << "FATAL DistributedDisplacementControl::domainChanged() - ran out of memory for";
-            opserr << " phat Vector of size " << size << endln;
-            exit(-1);
-        }
-    }
-
-    // now we have to determine phat
-    // do this by incrementing lambda by 1, applying load
-    // and getting phat from unbalance.
-    currentLambda = theModel->getCurrentDomainTime();
-
-    currentLambda += 1.0;
-    theModel->applyLoadDomain(currentLambda);    
-    
-    this->formUnbalance(*phat); // NOTE: this assumes unbalance at last was 0
-
-    currentLambda -= 1.0;
-    theModel->setCurrentDomainTime(currentLambda);    
-
-    // check there is a reference load
-    int haveLoad = 0;
-    for (int i=0; i<size; i++)
-      if ( (*phat)(i) != 0.0 ) {
-        haveLoad = 1;
-        i = size;
-      }
-
-    if (haveLoad == 0) {
-      opserr << "WARNING DistributedDisplacementControl::domainChanged() - zero reference load";
-      return -1;
-    }
+  if (haveLoad == 0) {
+    opserr << "WARNING DistributedDisplacementControl::domainChanged() - zero reference load";
+    return -1;
+  }
 
 
-    if (theDofID == -1) {
-      opserr << "DistributedDisplacementControl::setSize() - failed to find valid dof - are the node tag and dof values correct?\n";
-      return -1;
-    }
+  if (theDofID == -1) {
+    opserr << "DistributedDisplacementControl::setSize() - failed to find valid dof - are the node tag and dof values correct?\n";
+    return -1;
+  }
 
-    return 0;
+  return 0;
 }
 
 
